066d0147045ffae7e367cf5320ba27a479c6ed66
moriyaki <moriyaki999@gmail.com>
2025-11-13 09:45:46 +0900

    日付修正

---
+++ build_gakusei.bat
@@ -0,0 +1,3 @@
+@echo off
+wsl /${HOME}/novel_templete/make_epub.sh I_became_GAKUSEI
+"C:\Program Files (x86)\BPS\ChoTateGaki\BPSEPUBViewer.exe" c:\Users\moriyaki\OneDrive\novel\I_became_GAKUSEI\I_became_GAKUSEI.epub
